d1436ef9adfee7ad0c320e0f169ab5d3a7cde82c
Professeur64 <120922784+Professeur64@users.noreply.github.com>
2024-11-26 00:45:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,45 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+
+async function notifyTelegram(visitorInfo) {
+    const botToken = 'Y7671059018:AAF8t0J5wItwJ8wLfHMbPY7ZP_C_rAmjg_4'; // ضع هنا رمز البوت        https://api.telegram.org/bot7671059018:AAF8t0J5wItwJ8wLfHMbPY7ZP_C_rAmjg_4/getUpdates
+    const chatId = '6827736064'; // ضع هنا معرف الدردشة الخاصة بك
+    const message = `
+    🚶‍♂️ زائر جديد على الموقع:
+    - IP: ${visitorInfo.ip}
+    - المتصفح: ${visitorInfo.userAgent}
+    - الصفحة: ${visitorInfo.url}
+    `;
+    
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: message,
+        }),
+    });
+}
+
+// استدعاء الدالة عند تحميل الصفحة
+getVisitorInfo().then(visitorInfo => notifyTelegram(visitorInfo));
+
+
+
+async function getVisitorInfo() {
+    const response = await fetch('https://api.ipify.org?format=json');
+    const data = await response.json();
+    const visitorInfo = {
+        ip: data.ip,
+        userAgent: navigator.userAgent,
+        url: window.location.href,
+    };
+    return visitorInfo;
+}
   
